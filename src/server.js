e09d4b5b9eda881d3e5eac191fda1e5692cd36dc
komallsingh <singhkomal11711@gmail.com>
2026-09-03 23:34:04 +0530

    feat(websocket): added graceful shutdown method

--- src/server.js
+++ src/server.js
@@ -2,9 +2,43 @@ const app= require("./app");
 const http=require("http");
 const env=require("./config/env");
 const {createWebSocketServer}=require("./websocket/websocket.server");
-
 const server=http.createServer(app);
-createWebSocketServer(server);
+
+const wss=createWebSocketServer(server);
 server.listen(env.PORT,()=>{
     console.log(`Server is running on port ${env.PORT}`);
+});
+
+function shutdown(signal) {
+
+    console.log(
+        `${signal} received. Shutting down gracefully...`
+    );
+
+    // Stop accepting new HTTP connections
+    server.close(() => {
+
+        console.log("HTTP server closed");
+
+        // Close all WebSocket clients
+        for (const client of wss.clients) {
+            client.close();
+        }
+
+        // Close WebSocket server
+        wss.close(() => {
+
+            console.log("WebSocket server closed");
+
+            process.exit(0);
+        });
+    });
+}
+
+process.on("SIGINT", () => {
+    shutdown("SIGINT");
+});
+
+process.on("SIGTERM", () => {
+    shutdown("SIGTERM");
 });
